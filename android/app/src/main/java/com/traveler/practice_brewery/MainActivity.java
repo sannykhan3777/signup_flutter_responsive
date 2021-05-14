d37dbddf3a435ff046a239dfd829a9e173de7cbe
Sanny Khan <sannykhan3777@gmail.com>
2021-05-14 15:29:47 +0500

    final commit

---
+++ android/app/src/main/java/com/traveler/practice_brewery/MainActivity.java
@@ -0,0 +1,6 @@
+package com.traveler.practice_brewery;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool obscure_text = true;
+  Icon iconfirst = Icon(
+    Icons.visibility_off,
+    color: Colors.grey.shade300,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 50.0,
+                    backgroundImage: AssetImage("assets/images/sanny.jpg"),
+                  ),
+                  Text(
+                    "Sanaullah Abbasi",
+                    style: TextStyle(
+                        fontSize: 40.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Pacifico"),
+                  ),
+                  Text(
+                    "FLUTTER DEVELOPER",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal.shade100,
+                      fontFamily: "SourceSansPro",
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2.0,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                    width: 200,
+                    child: Divider(
+                      color: Colors.teal.shade100,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Container(
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    margin:
+                        EdgeInsets.symmetric(vertical: 0.0, horizontal: 50.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          Icons.call,
+                          color: Colors.teal.shade300,
+                        ),
+                        hintText: "Number",
+                        hintStyle: TextStyle(
+                          color: Colors.teal.shade300,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Container(
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    margin:
+                        EdgeInsets.symmetric(vertical: 0.0, horizontal: 50.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          Icons.email,
+                          color: Colors.teal.shade300,
+                        ),
+                        hintText: "Email",
+                        hintStyle: TextStyle(
+                          color: Colors.teal.shade300,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Container(
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    margin:
+                        EdgeInsets.symmetric(vertical: 0.0, horizontal: 50.0),
+                    child: TextField(
+                      obscureText: obscure_text,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Colors.teal.shade300,
+                        ),
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              if (obscure_text == true) {
+                                obscure_text = false;
+                                iconfirst = Icon(
+                                  Icons.visibility,
+                                  color: Colors.teal.shade300,
+                                );
+                              } else {
+                                obscure_text = true;
+                                iconfirst = Icon(
+                                  Icons.visibility_off,
+                                  color: Colors.grey.shade300,
+                                );
+                              }
+                            });
+                          },
+                          child: iconfirst,
+                        ),
+                        hintText: "Password",
+                        hintStyle: TextStyle(
+                          color: Colors.teal.shade300,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
